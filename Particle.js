60c2d7f6d18f1d46b5b73f5b967e4a3b33874eaa
Arne de Bree <arne@bukkie.nl>
2010-11-16 07:34:34 +0100

    push to gh_pages

--- Particle.js
+++ Particle.js
@@ -1,35 +1,81 @@
-function Particle( ctx, max ) {
+function Particle( ctx ) {
 
+    var STOPPED_VEL = 0.05; // velocity to be considerd as being stopped
+    
     var ctx = ctx,
-        canvas = ctx.canvas;
+        canvas = ctx.canvas,
+        cWidth = canvas.width,
+        cHeight = canvas.height;
         
-    this.max   = max;
-        
-    this.alpha  = 1;
+    this.keepWhenstopped = false;
+    
+    this.fov   = 250;
+    
+    this.grav  = 0.98;
+    
+    this.bounce = 0.9;
+    
+    this.xDrag = 1;
+    this.yDrag = 1;
+    this.zDrag = 1;
+    
+    this.xVel  = 0;
+    this.yVel  = 0;
+    this.zVel  = 0;
+    
+    this.x     = 0;
+    this.y     = 0;
+    this.z     = 0; 
+    
+    this.color = "rgb( "+ parseInt( Math.random() * 200 + 55 ) +", "+ parseInt( Math.random() * 200 + 55 ) +","+ parseInt(  Math.random() * 200 + 55 ) +" )";;
+    this.lineWidth = 3;
+    
+    this.update = function()
+    {
+        this.xVel *= this.xDrag;
 
-    this.point;    
-    
-    this.color = "rgba( "+ Math.round( rand( 50, 255 ) ) +", "+ Math.round( rand( 50, 255 ) ) +","+ Math.round( rand( 50, 255 ) ) +","+ this.alpha +" )";;
-    
-    this.rotateX = function( angle )
-    {
-        this.point.rotateX( angle );
-    }    
-    
-    this.rotateY = function( angle )
-    {
-        this.point.rotateY( angle );    
-    }
-    
-    this.rotateZ = function( angle )
-    {
-        this.point.rotateZ( angle );    
-    }
-    
+        this.yVel *= this.yDrag;                
+        this.yVel += this.grav;
+
+
+        this.x += this.xVel;
+        this.y += this.yVel;        
+        
+        if ( this.y > cHeight && this.yVel > this.grav )
+        {
+            this.y = cHeight;
+            this.yVel *= -this.bounce;
+        }
+        
+    };
     
     this.draw = function()
     {
-        this.point.draw( ctx, this.color, this.max );    
+        ctx.lineWidth = this.lineWidth;
+        ctx.strokeStyle = this.color;
+        
+        var scale = this.fov / ( this.fov + this.z );
+        
+        var x2d = this.x * scale;
+        var y2d = this.y * scale;
+
+        ctx.beginPath();
+        ctx.moveTo( x2d, y2d );
+        ctx.lineTo( x2d + this.lineWidth, y2d );
+        
+        ctx.stroke();
     };
     
+    this.isStopped = function()
+    {
+        if ( this.keepWhenStopped )
+        {
+            return false;
+        }
+    
+        return ( 
+            ( this.xVel < STOPPED_VEL && this.xVel > -STOPPED_VEL && this.yVel < STOPPED_VEL && this.yVel > -STOPPED_VEL ) ||
+            ( this.x < 0 || this.x > cWidth || this.y < 0 || this.y > cHeight )
+        );
+    }
 }
